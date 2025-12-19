08c451aba1c1ef8d1afc95eab1d7cf5224c77475
Hareesh-BBC <hareesh.s@brownboxconsulting.com>
2025-12-19 12:25:11 +0530

    Add Playwright automation setup

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from '@playwright/test';
+
+export default defineConfig({
+  timeout: 60000,
+  use: {
+    headless: true,
+    screenshot: 'only-on-failure',
+    trace: 'retain-on-failure',
+  },
+  workers: 1,
+  reporter: [],
+  projects: [
+    { name: 'main', use: { browserName: 'chromium', baseURL: 'https://perflow.brownboxconsulting.com' } },
+    { name: 'develop', use: { browserName: 'chromium', baseURL: 'https://preview--perflow.lovable.app/auth' } },
+    { name: 'qa', use: { browserName: 'chromium', baseURL: 'https://preview--perflow.lovable.app/auth' } }
+  ],
+});
